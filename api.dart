3774d0ac59a63bfa614f2bc8a650ae3258712a6b
rakishraja <124429981+rakishraja@users.noreply.github.com>
2023-07-30 22:14:10 +0530

    Delete api.dart

--- api.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dio/dio.dart';
-Future<String> callApi(dynamic requestData,int num) async {
-  Dio client = Dio();
-  var urlPath = 'https://waterquality-tkjy.onrender.com/water_quality';
-  var response = await client.post(urlPath, data: requestData,queryParameters: {"mdl":num});
-  String output = response.data.toString();
-  return output;
-}
