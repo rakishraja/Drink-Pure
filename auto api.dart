b9c770330aa0665ea4ac33ee2694ef995adeff8c
rakishraja <124429981+rakishraja@users.noreply.github.com>
2023-07-30 22:14:27 +0530

    Delete auto api.dart

--- auto api.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dio/dio.dart';
-
-Future<Map<String, dynamic>> autoFill(dynamic requestData) async {
-  Dio client = Dio();
-  var urlPath = 'https://wateraf.onrender.com/Auto_fill';
-  var response = await client.post(urlPath, data:requestData);
-  Map<String, dynamic> output = response.data;
-  return output;
-}
